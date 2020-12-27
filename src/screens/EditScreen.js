eb3ff799b9983932079fd8930c625f2112e8b649
Noel Kirkland <noelkirkland@gmail.com>
2020-12-27 14:18:29 -0800

    create EditScreen

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,15 @@
+import React, { useContext, useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+
+const EditScreen = () => {
+
+return (
+    <View>
+      <Text>Edit Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default EditScreen;
